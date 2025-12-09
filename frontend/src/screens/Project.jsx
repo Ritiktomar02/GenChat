6c7bbf48c41d2f7337e4605915c36594bf6e300f
Ritik Tomar <ritiktomar@Ritiks-MacBook-Air.local>
2025-12-09 14:51:04 +0530

    project model

--- frontend/src/screens/Project.jsx
+++ frontend/src/screens/Project.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
-
+import { useLocation } from 'react-router-dom'
 const Project = () => {
+  const location=useLocation();
+  console.log(location.state)
   return (
     <div>
       Project
